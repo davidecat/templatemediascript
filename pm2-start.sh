7f38dbe83b3dfeae5710c8497264f15126a6856c
sandeepraomarathacs <33785900+sandeepraomarathacs@users.noreply.github.com>
2026-05-19 03:59:19 +0530

    Fix pm2-start: use false daemonize for PM2 foreground

--- pm2-start.sh
+++ pm2-start.sh
@@ -7,7 +7,7 @@ cd "$ROOT"
 LIQ_BIN="${LIQ_BIN:-/usr/bin/liquidsoap}"
 SCRIPT="${SCRIPT:-$ROOT/script.liq}"
 USER_ID="${LIQ_USER_ID:-user1}"
-API_TOKEN="${LIQ_API_TOKEN:-}"
-CONTROL_PORT="${LIQ_CONTROL_PORT:-7001}"
+# script.liq argv(2) must be "true" or "false" (daemonize). PM2 needs foreground.
+DAEMONIZE="${LIQ_DAEMONIZE:-false}"
 
-exec "$LIQ_BIN" "$SCRIPT" -- "$USER_ID" "true" "$CONTROL_PORT"
+exec "$LIQ_BIN" "$SCRIPT" -- "$USER_ID" "$DAEMONIZE"
